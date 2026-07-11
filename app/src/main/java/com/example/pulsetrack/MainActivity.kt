5131a8c7a773ac1c5f40214140e2b4ef31352701
Purba Indra <purboindra@gmail.com>
2026-07-11 20:53:03 +0700

    fix: setup AppRoute MainActivity.kt

--- app/src/main/java/com/example/pulsetrack/MainActivity.kt
+++ app/src/main/java/com/example/pulsetrack/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.pulsetrack.core.navigation.AppRoute
 import com.example.pulsetrack.ui.theme.PulseTrackTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PulseTrackTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppRoute()
             }
         }
     }
