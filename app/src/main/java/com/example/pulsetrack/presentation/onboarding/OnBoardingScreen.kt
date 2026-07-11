e221ac0d5a0bfa2e233bc8cf26ee7c08af911d57
Purba Indra <purboindra@gmail.com>
2026-07-11 20:52:17 +0700

    feat: add onboarding screen

---
+++ app/src/main/java/com/example/pulsetrack/presentation/onboarding/OnBoardingScreen.kt
@@ -0,0 +1,75 @@
+package com.example.pulsetrack.presentation.onboarding
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import com.example.pulsetrack.R
+import com.example.pulsetrack.ui.theme.PulseTrackTheme
+
+@Composable
+fun OnBoardingScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { paddingValues ->
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(PulseTrackTheme.spacing.base)
+        ) {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                style = MaterialTheme.typography.headlineMedium,
+                color = MaterialTheme.colorScheme.primary,
+            )
+            Spacer(modifier = Modifier.height(PulseTrackTheme.spacing.sm))
+            Text(
+                stringResource(R.string.app_description),
+                style = TextStyle(
+                    color = MaterialTheme.colorScheme.outline
+                )
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Image(
+                painter = painterResource(id = R.drawable.pulse_track_splash),
+                contentDescription = "Splash",
+                modifier = Modifier.size(424.dp),
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            ElevatedButton(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentPadding = PaddingValues(PulseTrackTheme.spacing.md),
+                colors = ButtonDefaults.elevatedButtonColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                ),
+            ) {
+                Text(
+                    "Start Running",
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        color = MaterialTheme.colorScheme.surface
+                    )
+                )
+            }
+        }
+    }
+}
