8eeedcbe1c3e96069116bc7b0ea14d08ca77021d
Purba Indra <purboindra@gmail.com>
2026-07-14 06:45:20 +0700

    feat: xxs Spacing

--- app/src/main/java/com/example/pulsetrack/ui/theme/Spacing.kt
+++ app/src/main/java/com/example/pulsetrack/ui/theme/Spacing.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 data class PulseTrackSpacing(
+    val xxs: Dp = 2.dp,
     val xs: Dp = 4.dp,
     val sm: Dp = 8.dp,
     val md: Dp = 16.dp,
