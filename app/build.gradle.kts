0a8468ba508ba9d5586d00121abba9396b2aa3e5
Purba Indra <purboindra@gmail.com>
2026-07-14 06:45:49 +0700

    chore: grid compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
     implementation(libs.material.icons.extended)
+    implementation(libs.androidx.compose.foundation.layout)
 
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.cio)
