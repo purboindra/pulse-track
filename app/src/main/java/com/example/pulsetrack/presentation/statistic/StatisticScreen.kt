869ab71d41b876f3198c3818923588e3312eeb52
purboindra <purboindra@gmail.com>
2026-07-26 17:04:12 +0700

    refactor: modularize StatisticScreen components

--- app/src/main/java/com/example/pulsetrack/presentation/statistic/StatisticScreen.kt
+++ app/src/main/java/com/example/pulsetrack/presentation/statistic/StatisticScreen.kt
@@ -1,6 +1,7 @@
 package com.example.pulsetrack.presentation.statistic
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,14 +9,22 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.pulsetrack.ui.theme.PulseTrackTheme
+import com.example.pulsetrack.ui.theme.PureWhite
+
+
+
 
 @Composable
 fun StatisticScreen(modifier: Modifier = Modifier) {
@@ -25,37 +34,16 @@ fun StatisticScreen(modifier: Modifier = Modifier) {
         ),
     ) {
         item {
-            Column() {
-                Text(
-                    "Weekly Progress", style = MaterialTheme.typography.labelSmall.copy(
-                        color = MaterialTheme.colorScheme.outline
-                    )
-                )
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Row {
-                        Text("15Km", style = MaterialTheme.typography.titleMedium)
-                        Text(
-                            " / 30km goal", style = MaterialTheme.typography.titleMedium.copy(
-                                color = MaterialTheme.colorScheme.outline
-                            )
-                        )
-                    }
-                    Text(
-                        "50% Complete", style = MaterialTheme.typography.labelSmall.copy(
-                            color = MaterialTheme.colorScheme.primary
-                        )
-                    )
-                }
-                Spacer(modifier = Modifier.height(PulseTrackTheme.spacing.sm))
-                Card(
-                    modifier = Modifier.height(230.dp).fillMaxWidth(),
-
-                ) { }
-            }
+            WeeklyProgress()
+        }
+        item {
+            StatisticCardSection()
+        }
+        item {
+           MonthlyActivity()
+        }
+        item {
+            TotalAchievementCard()
         }
     }
 }
