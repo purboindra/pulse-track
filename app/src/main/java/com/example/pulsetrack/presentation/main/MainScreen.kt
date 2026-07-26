352e3118e54c24d4afe1ae4e75859e79b86309ad
purboindra <purboindra@gmail.com>
2026-07-27 06:58:24 +0700

    feat: invoke ProfileScreen at MainScreen.kt

--- app/src/main/java/com/example/pulsetrack/presentation/main/MainScreen.kt
+++ app/src/main/java/com/example/pulsetrack/presentation/main/MainScreen.kt
@@ -1,40 +1,27 @@
 package com.example.pulsetrack.presentation.main
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
-import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.ui.NavDisplay
@@ -42,9 +29,8 @@ import com.example.pulsetrack.core.navigation.BottomNavbar
 import com.example.pulsetrack.presentation.component.AppBar
 import com.example.pulsetrack.presentation.history.HistoryScreen
 import com.example.pulsetrack.presentation.home.HomeScreen
+import com.example.pulsetrack.presentation.profile.ProfileScreen
 import com.example.pulsetrack.presentation.statistic.StatisticScreen
-import kotlinx.coroutines.launch
-import kotlin.collections.mutableSetOf
 
 
 private val bottomNavbarItems = listOf(
@@ -117,7 +103,7 @@ fun MainScreen() {
             entryProvider = { key ->
                 when (key) {
                     is BottomNavbar.Home -> NavEntry(key) {
-                        HomeScreen()
+                        HomeScreen(modifier = Modifier.fillMaxSize())
                     }
 
                     is BottomNavbar.Run -> NavEntry(key) {
@@ -125,7 +111,7 @@ fun MainScreen() {
                     }
 
                     is BottomNavbar.Profile -> NavEntry(key) {
-                        Text("Profile Screen")
+                        ProfileScreen(modifier = Modifier.fillMaxSize())
                     }
 
 
