aa9fb9aba0d3f8b1073d6652a4817e3feebde422
purboindra <purboindra@gmail.com>
2026-07-29 06:56:48 +0700

    todo: implement osmdroid

--- app/src/main/java/com/example/pulsetrack/MainApplication.kt
+++ app/src/main/java/com/example/pulsetrack/MainApplication.kt
@@ -1,12 +1,18 @@
 package com.example.pulsetrack
 
 import android.app.Application
+import android.preference.PreferenceManager
 import com.example.pulsetrack.di.databaseModule
 import org.koin.core.context.startKoin
+import org.osmdroid.config.Configuration
 
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+        Configuration.getInstance().userAgentValue = "PulseTrackApp/1.0 (com.example.pulsetrack)"
+
         startKoin {
             modules(
                 databaseModule,
--- app/src/main/java/com/example/pulsetrack/presentation/run/RunScreen.kt
+++ app/src/main/java/com/example/pulsetrack/presentation/run/RunScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Pause
@@ -28,102 +29,117 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pulsetrack.presentation.component.AppButton
+import com.example.pulsetrack.presentation.component.OsmMapView
 import com.example.pulsetrack.ui.theme.PulseTrackTheme
 
 @OptIn(ExperimentalGridApi::class)
 @Composable
 
 fun RunScreen(modifier: Modifier = Modifier) {
+  
     Column(
         modifier = modifier.padding(
             PulseTrackTheme.spacing.md
         ),
     ) {
-        Column(
-            modifier = Modifier.fillMaxWidth(),
+        OsmMapView(
+            modifier = Modifier
+                .height(240.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(PulseTrackTheme.spacing.base))
+        )
+        Spacer(modifier = Modifier.height(PulseTrackTheme.spacing.base))
+        LazyColumn(
+            modifier = Modifier.weight(1f),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                "DURATION", style = MaterialTheme.typography.labelSmall.copy(
-                    fontSize = 11.sp,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant
-                )
-            )
-            Spacer(modifier = Modifier.height(PulseTrackTheme.spacing.xs))
-            Text(
-                "00:45:14", style = MaterialTheme.typography.headlineLarge.copy(
-                    fontSize = 48.sp,
-                )
-            )
-        }
-        Spacer(modifier = Modifier.height(PulseTrackTheme.spacing.lg))
-        Grid(
-            config = {
-                repeat(2) {
-                    column(171.dp)
+            item {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        "DURATION", style = MaterialTheme.typography.labelSmall.copy(
+                            fontSize = 11.sp,
+                            color = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    )
+                    Spacer(modifier = Modifier.height(PulseTrackTheme.spacing.xs))
+                    Text(
+                        "00:45:14", style = MaterialTheme.typography.headlineLarge.copy(
+                            fontSize = 48.sp,
+                        )
+                    )
                 }
-                repeat(2) {
-                    row(96.dp)
+                Spacer(modifier = Modifier.height(PulseTrackTheme.spacing.lg))
+                Grid(
+                    config = {
+                        repeat(2) {
+                            column(171.dp)
+                        }
+                        repeat(2) {
+                            row(96.dp)
+                        }
+                        gap(8.dp)
+                    }
+                ) {
+                    StatusRunCard(
+                        label = "DISTANCE",
+                        value = "8.2",
+                        unit = "km"
+                    )
+                    StatusRunCard(
+                        label = "PACE",
+                        value = "5:32",
+                        unit = "/km"
+                    )
+                    StatusRunCard(
+                        label = "AVG PACE",
+                        value = "5:40",
+                        unit = "/km"
+                    )
+                    StatusRunCard(
+                        label = "CALORIES",
+                        value = "450",
+                        unit = "kcal"
+                    )
                 }
-                gap(8.dp)
-            }
-        ) {
-            StatusRunCard(
-                label = "DISTANCE",
-                value = "8.2",
-                unit = "km"
-            )
-            StatusRunCard(
-                label = "PACE",
-                value = "5:32",
-                unit = "/km"
-            )
-            StatusRunCard(
-                label = "AVG PACE",
-                value = "5:40",
-                unit = "/km"
-            )
-            StatusRunCard(
-                label = "CALORIES",
-                value = "450",
-                unit = "kcal"
-            )
-        }
-        Spacer(modifier = Modifier.height(PulseTrackTheme.spacing.lg))
-        Row(
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            AppButton(
-                onClick = {}, label = "PAUSE", modifier = Modifier
-                    .weight(1f)
-                    .height(72.dp),
-                shape = RoundedCornerShape(PulseTrackTheme.spacing.base)
-            )
-            Spacer(modifier = Modifier.width(PulseTrackTheme.spacing.sm))
-            Box(
-                modifier = Modifier
-                    .size((72.dp))
-                    .border(
-                        width = 1.dp,
-                        color = MaterialTheme.colorScheme.tertiaryContainer,
+                Spacer(modifier = Modifier.height(PulseTrackTheme.spacing.lg))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    AppButton(
+                        onClick = {}, label = "PAUSE", modifier = Modifier
+                            .weight(1f)
+                            .height(72.dp),
                         shape = RoundedCornerShape(PulseTrackTheme.spacing.base)
                     )
-                    .clip(
-                        RoundedCornerShape(PulseTrackTheme.spacing.base)
-                    )
-                    .background(
-                        Color(0xffFFB3AD)
-                    )
-                    .clickable(
-                        onClick = {}
-                    ),
-                contentAlignment = Alignment.Center
-            ) {
-                Icon(
-                    Icons.Default.Pause,
-                    contentDescription = "",
-                    tint = MaterialTheme.colorScheme.onTertiaryContainer
-                )
+                    Spacer(modifier = Modifier.width(PulseTrackTheme.spacing.sm))
+                    Box(
+                        modifier = Modifier
+                            .size((72.dp))
+                            .border(
+                                width = 1.dp,
+                                color = MaterialTheme.colorScheme.tertiaryContainer,
+                                shape = RoundedCornerShape(PulseTrackTheme.spacing.base)
+                            )
+                            .clip(
+                                RoundedCornerShape(PulseTrackTheme.spacing.base)
+                            )
+                            .background(
+                                Color(0xffFFB3AD)
+                            )
+                            .clickable(
+                                onClick = {}
+                            ),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            Icons.Default.Pause,
+                            contentDescription = "",
+                            tint = MaterialTheme.colorScheme.onTertiaryContainer
+                        )
+                    }
+                }
             }
         }
     }
