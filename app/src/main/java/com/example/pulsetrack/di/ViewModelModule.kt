9300b29e4f18929f4258f4c6861110083828a8e1
purboindra <purboindra@gmail.com>
2026-07-31 06:52:26 +0700

    feat: add HomeViewModel to ViewModelModule

--- app/src/main/java/com/example/pulsetrack/di/ViewModelModule.kt
+++ app/src/main/java/com/example/pulsetrack/di/ViewModelModule.kt
@@ -1,5 +1,6 @@
 package com.example.pulsetrack.di
 
+import com.example.pulsetrack.presentation.home.HomeViewModel
 import com.example.pulsetrack.presentation.run.RunViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
@@ -7,5 +8,8 @@ import org.koin.dsl.module
 val viewModelModule = module {
     viewModel {
         RunViewModel(get())
+    }
+    viewModel {
+        HomeViewModel()
     }
 }
