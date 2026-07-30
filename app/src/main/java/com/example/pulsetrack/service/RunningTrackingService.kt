e627c41512de868fd3ecfb043cfcdac862ff4deb
purboindra <purboindra@gmail.com>
2026-07-30 21:43:46 +0700

    feat: implement location tracking in RunningTrackingService

--- app/src/main/java/com/example/pulsetrack/service/RunningTrackingService.kt
+++ app/src/main/java/com/example/pulsetrack/service/RunningTrackingService.kt
@@ -6,10 +6,17 @@ import android.app.Service
 import android.content.Intent
 import android.os.Build
 import android.os.IBinder
+import android.os.Looper
+import androidx.core.app.NotificationCompat
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import org.osmdroid.util.GeoPoint
 
 class RunningTrackingService : Service() {
@@ -32,11 +39,56 @@ class RunningTrackingService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return super.onStartCommand(intent, flags, startId)
-
-
+        when (intent?.action) {
+            ACTION_START -> startTracking()
+            ACTION_STOP -> stopTracking()
+        }
+        return START_STICKY
     }
 
+    private fun startTracking() {
+        createNotificationChannel()
+        val notification = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setContentTitle("PulseTrack Running")
+            .setContentText("Tracking your run...")
+            .setSmallIcon(android.R.drawable.ic_menu_compass)
+            .setOngoing(true)
+            .build()
+        startForeground(NOTIFICATION_ID, notification)
+        requestLocationUpdates()
+    }
+
+    private fun requestLocationUpdates() {
+        val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 2000L)
+            .setMinUpdateIntervalMillis(1000L).build()
+
+        try {
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallBack,
+                Looper.getMainLooper()
+            )
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+    }
+
+    private val locationCallBack = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult) {
+            result.locations.forEach { location ->
+                val newPoint = GeoPoint(location.latitude, location.longitude)
+                _pathPoints.update {
+                    _pathPoints.value + newPoint
+                }
+            }
+        }
+    }
+
+    private fun stopTracking() {
+        fusedLocationClient.removeLocationUpdates(locationCallBack)
+        stopForeground(STOP_FOREGROUND_REMOVE)
+        stopSelf()
+    }
 
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
