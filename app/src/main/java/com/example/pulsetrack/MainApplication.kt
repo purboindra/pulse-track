11f0f1d7c87680e8c5b1169bc2f80593bb16eebf
purboindra <purboindra@gmail.com>
2026-07-30 21:43:32 +0700

    chore: configure koin modules and context

--- app/src/main/java/com/example/pulsetrack/MainApplication.kt
+++ app/src/main/java/com/example/pulsetrack/MainApplication.kt
@@ -3,7 +3,12 @@ package com.example.pulsetrack
 import android.app.Application
 import android.preference.PreferenceManager
 import com.example.pulsetrack.di.databaseModule
+import com.example.pulsetrack.di.serviceModule
+import com.example.pulsetrack.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 import org.osmdroid.config.Configuration
 
 class MainApplication : Application() {
@@ -14,8 +19,12 @@ class MainApplication : Application() {
 //        Configuration.getInstance().userAgentValue = "PulseTrackApp/1.0 (com.example.pulsetrack)"
 
         startKoin {
+            androidContext(this@MainApplication)
+            androidLogger(Level.DEBUG)
             modules(
                 databaseModule,
+                serviceModule,
+                viewModelModule
             )
         }
     }
