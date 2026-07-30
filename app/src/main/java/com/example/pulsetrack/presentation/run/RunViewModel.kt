33b9a6b92651eeca6347dd061f78967d9c890eb9
purboindra <purboindra@gmail.com>
2026-07-31 06:52:21 +0700

    feat: track run state in RunViewModel

--- app/src/main/java/com/example/pulsetrack/presentation/run/RunViewModel.kt
+++ app/src/main/java/com/example/pulsetrack/presentation/run/RunViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.pulsetrack.presentation.run
 
 import androidx.lifecycle.ViewModel
+import com.example.pulsetrack.presentation.state.RunState
 import com.example.pulsetrack.service.RunningServiceController
 import com.example.pulsetrack.service.RunningTrackingService
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import org.osmdroid.util.GeoPoint
 
 class RunViewModel(
@@ -12,11 +15,25 @@ class RunViewModel(
 ) : ViewModel() {
     val pathPoints: StateFlow<List<GeoPoint>> = RunningTrackingService.pathPoints
 
+    private val _runState = MutableStateFlow(RunState())
+    val runState = _runState.asStateFlow()
+
     fun onStartRunClicked() {
         runningServiceController.startTracking()
+        _runState.update {
+            it.copy(
+                isTracking = true
+            )
+        }
     }
 
     fun onStopRunClicked() {
         runningServiceController.stopTracking()
+        _runState.update {
+            it.copy(
+                isPaused = true,
+                isTracking = false
+            )
+        }
     }
 }
