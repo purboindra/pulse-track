7627115447031e25e168eb180fa441833598152a
purboindra <purboindra@gmail.com>
2026-07-31 06:51:45 +0700

    refactor: use createDatabase in DatabaseModule

--- app/src/main/java/com/example/pulsetrack/di/DatabaseModule.kt
+++ app/src/main/java/com/example/pulsetrack/di/DatabaseModule.kt
@@ -1,9 +1,9 @@
 package com.example.pulsetrack.di
 
 import com.example.pulsetrack.data.local.database.AppDatabase
+import com.example.pulsetrack.data.local.database.createDatabase
 import org.koin.dsl.module
-import org.koin.plugin.module.dsl.single
 
 val databaseModule = module {
-    single<AppDatabase>()
+    single { createDatabase(get()) }
 }
