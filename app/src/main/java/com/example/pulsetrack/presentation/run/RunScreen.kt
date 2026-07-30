2bfba15a528743b6751ffc70b308bf771aa316c7
purboindra <purboindra@gmail.com>
2026-07-30 21:43:51 +0700

    feat: add location permission request to RunScreen

--- app/src/main/java/com/example/pulsetrack/presentation/run/RunScreen.kt
+++ app/src/main/java/com/example/pulsetrack/presentation/run/RunScreen.kt
@@ -1,5 +1,10 @@
 package com.example.pulsetrack.presentation.run
 
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -22,12 +27,20 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
 import com.example.pulsetrack.presentation.component.AppButton
 import com.example.pulsetrack.presentation.component.OsmMapView
 import com.example.pulsetrack.ui.theme.PulseTrackTheme
@@ -37,8 +50,34 @@ import com.example.pulsetrack.ui.theme.PulseTrackTheme
 
 fun RunScreen(modifier: Modifier = Modifier) {
 
+    val context = LocalContext.current
+    var hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
 
-  
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        hasLocationPermission = permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true ||
+                permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+    }
+
+    LaunchedEffect(Unit) {
+        if (!hasLocationPermission) {
+            permissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                )
+            )
+        }
+    }
+
     Column(
         modifier = modifier.padding(
             PulseTrackTheme.spacing.md
