b3fd2ac8cb88f05112e834763df48d9e36ed83b2
purboindra <purboindra@gmail.com>
2026-08-03 06:43:00 +0700

    feat: add formatDuration helper

--- app/src/main/java/com/example/pulsetrack/core/helpers/DurationHelper.kt
+++ app/src/main/java/com/example/pulsetrack/core/helpers/DurationHelper.kt
@@ -1,5 +1,6 @@
 package com.example.pulsetrack.core.helpers
 
+import android.annotation.SuppressLint
 import java.util.Locale
 
 object DurationHelper {
@@ -7,6 +8,25 @@ object DurationHelper {
         val hours = seconds / 3600
         val minutes = (seconds % 3600) / 60
         val remainingSeconds = seconds % 60
-        return String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, remainingSeconds)
+        return String.format(
+            Locale.getDefault(),
+            "%02d:%02d:%02d",
+            hours,
+            minutes,
+            remainingSeconds
+        )
+    }
+
+    @SuppressLint("DefaultLocale")
+    fun formatDuration(totalSeconds: Long): String {
+        val hours = totalSeconds / 3600
+        val minutes = (totalSeconds % 3600) / 60
+        val seconds = totalSeconds % 60
+
+        return if (hours > 0) {
+            String.format("%02d:%02d:%02d", hours, minutes, seconds)
+        } else {
+            String.format("%02d:%02d", minutes, seconds)
+        }
     }
 }
